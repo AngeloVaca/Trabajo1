0f026557f5c50eb6282c95a8ca3ccfb3925513c8
Jordi7-7 <jordi.cahuas20@gmail.com>
2023-04-21 12:44:52 -0500

    aaaalaaaaa anchelo

--- src/com/varios.java
+++ src/com/varios.java
@@ -14,17 +14,7 @@ public class varios {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Jordi GAY");
-        System.out.println("Jordi GAY");
-        System.out.println("Jordi GAY");
-        System.out.println("Jordi GAY");
-        System.out.println("Jordi GAY");
-        System.out.println("Jordi GAY");
-        System.out.println("Jordi GAY");
-        System.out.println("Jordi GAY");
-        System.out.println("Jordi GAY");
-        System.out.println("Jordi GAY");
-        System.out.println("Jordi GAY");
+        System.out.println("noce");
     }
     
 }
